e61de5cdbbadf003980580d92bc28f4a70f8f0d8
mati <mateusz.labuda@voicetel.pl>
2020-05-05 22:10:12 +0200

    optimalizations nn and preprocessing

--- tweet_sentiment/tweet_sent_predict.py
+++ tweet_sentiment/tweet_sent_predict.py
@@ -49,7 +49,7 @@ def preprocess_text(sen):
     return sentence
 
 # https://www.kaggle.com/kazanova/sentiment140
-dataset = pd.read_csv("/home/mati/nlpmgr/tweet_sentiment/training.1600000.processed.noemoticon.csv",
+dataset = pd.read_csv("/home/mati/nlp_mgr/tweet_sentiment/training.1600000.processed.noemoticon.csv",
                          header=None,
                          skiprows=790000, nrows=20000,
                         #  skiprows=500000, nrows=600000,
@@ -99,110 +99,10 @@ for sen in X1_test_sentences:
 
 X1_test_clean = list(X1_test)
 
-maxlen = 100
-""" Keras Tokenizer """
-tokenizer = Tokenizer(num_words=10000)
-tokenizer.fit_on_texts(X1_train)
-# print(tokenizer.word_index)
-X1_train = tokenizer.texts_to_sequences(X1_train)
-X1_test = tokenizer.texts_to_sequences(X1_test)
-vocab_size = len(tokenizer.word_index) + 1
-
-X1_train = pad_sequences(X1_train, padding='post', maxlen=maxlen)
-X1_test = pad_sequences(X1_test, padding='post', maxlen=maxlen)
-
-""" TreebankWordTokenizer NLTK """
-"""
-word_index = 1
-nltk_tokenizer_word_dict = {}
-X3_train = []
-X3_test = []
-for sen in X1_train_clean:
-    sentence_seq = []
-    sen_tokens = word_tokenize(sen)
-    for token in sen_tokens:
-        if token not in nltk_tokenizer_word_dict:
-            nltk_tokenizer_word_dict[token] = word_index
-            word_index += 1
-
-        sentence_seq.append(nltk_tokenizer_word_dict[token])
-    X3_train.append(sentence_seq)
-
-vocab_size_3 = len(nltk_tokenizer_word_dict) + 1
-
-unknown_words_in_test = set()
-all_words_in_test = set()
-for sen in X1_test_clean:
-    sentence_seq = []
-    sen_tokens = word_tokenize(sen)
-    for token in sen_tokens:
-        all_words_in_test.add(token)
-        try:
-            sentence_seq.append(nltk_tokenizer_word_dict[token])
-        except:
-            unknown_words_in_test.add(token)
-            # jesli nie znam słowa nic nie dodaje
-    X3_test.append(sentence_seq)
-
-print("Ilość unik. nauczonych słów z train:", len(all_words_in_test))
-print("Nieznane słowa z test:", len(unknown_words_in_test))
-# sys.exit(0)
-"""
-######################
-
-
-""" TweetTokenizer NLTK """
-word_index = 1
-tweet_tokenizer_word_dict = {}
-X2_train = []
-X2_test = []
-tweettokenizer = TweetTokenizer(strip_handles=True, reduce_len=True)
-for sen in X1_train_clean:
-    sentence_seq = []
-    sen_tokens = tweettokenizer.tokenize(sen)
-    for token in sen_tokens:
-        if token not in tweet_tokenizer_word_dict:
-            tweet_tokenizer_word_dict[token] = word_index
-            word_index += 1
-
-        sentence_seq.append(tweet_tokenizer_word_dict[token])
-    X2_train.append(sentence_seq)
-
-vocab_size_2 = len(tweet_tokenizer_word_dict) + 1
-print(tweet_tokenizer_word_dict)
-
-unknown_words_in_test = set()
-all_words_in_test = set()
-for sen in X1_test_clean:
-    sentence_seq = []
-    sen_tokens = tweettokenizer.tokenize(sen)
-    for token in sen_tokens:
-        all_words_in_test.add(token)
-        try:
-            sentence_seq.append(tweet_tokenizer_word_dict[token])
-        except:
-            unknown_words_in_test.add(token)
-            # jesli nie znam słowa nic nie dodaje
-    X2_test.append(sentence_seq)
-
-print("Ilość unik. nauczonych słów z train:", len(all_words_in_test))
-print("Nieznane słowa z test:", len(unknown_words_in_test))
-# sys.exit(0)
-########################
-
-
-# print("Word Dict : ",tweet_tokenizer_word_dict[:50])
-# print("Word seq : ",word_seq)
-
-X2_train = pad_sequences(X2_train, padding='post', maxlen=maxlen)
-X2_test = pad_sequences(X2_test, padding='post', maxlen=maxlen)
-
-# X3_train = pad_sequences(X3_train, padding='post', maxlen=maxlen)
-# X3_test = pad_sequences(X3_test, padding='post', maxlen=maxlen)
 
 embeddings_dictionary = dict()
 # http://nlp.stanford.edu/data/wordvecs/glove.twitter.27B.zip
-glove_file = open('/home/mati/nlpmgr/glove/glove.twitter.27B/glove.twitter.27B.100d.txt', encoding="utf8")
+glove_file = open('/home/mati/nlp_mgr/glove/glove.twitter.27B/glove.twitter.27B.100d.txt', encoding="utf8")
 for line in glove_file:
     records = line.split()
     word = records[0]
@@ -211,55 +111,165 @@ for line in glove_file:
 
 glove_file.close()
 
+maxlen = 50
+chosen_tokenizer = 'tbwt'
+if chosen_tokenizer == 'keras':
+    """ Keras Tokenizer """
+    # """
+    tokenizer = Tokenizer(num_words=10000)
+    tokenizer.fit_on_texts(X1_train)
+    # print(tokenizer.word_index)
+    X1_train = tokenizer.texts_to_sequences(X1_train)
+    X1_test = tokenizer.texts_to_sequences(X1_test)
+    vocab_size = len(tokenizer.word_index) + 1
 
-# """ Keras tokenizer embedding """
-# embedding_matrix = zeros((vocab_size, 100))
-# for word, index in tokenizer.word_index.items():
-#     embedding_vector = embeddings_dictionary.get(word)
-#     if embedding_vector is not None:
-#         embedding_matrix[index] = embedding_vector
+    X1_train = pad_sequences(X1_train, padding='post', maxlen=maxlen)
+    X1_test = pad_sequences(X1_test, padding='post', maxlen=maxlen)
+
+    """ Keras tokenizer embedding """
+    embedding_matrix = zeros((vocab_size, 100))
+    for word, index in tokenizer.word_index.items():
+        embedding_vector = embeddings_dictionary.get(word)
+        if embedding_vector is not None:
+            embedding_matrix[index] = embedding_vector
+
+    ###########
+    # """
+elif chosen_tokenizer == 'tbwt':
+    """ TreebankWordTokenizer NLTK """
+    word_index = 1
+    nltk_tokenizer_word_dict = {}
+    X3_train = []
+    X3_test = []
+    for sen in X1_train_clean:
+        sentence_seq = []
+        sen_tokens = word_tokenize(sen)
+        for token in sen_tokens:
+            if token not in nltk_tokenizer_word_dict:
+                nltk_tokenizer_word_dict[token] = word_index
+                word_index += 1
+
+            sentence_seq.append(nltk_tokenizer_word_dict[token])
+        X3_train.append(sentence_seq)
+
+    vocab_size_3 = len(nltk_tokenizer_word_dict) + 1
+
+    unknown_words_in_test = set()
+    all_words_in_test = set()
+    for sen in X1_test_clean:
+        sentence_seq = []
+        sen_tokens = word_tokenize(sen)
+        for token in sen_tokens:
+            all_words_in_test.add(token)
+            try:
+                sentence_seq.append(nltk_tokenizer_word_dict[token])
+            except:
+                unknown_words_in_test.add(token)
+                # jesli nie znam słowa nic nie dodaje
+        X3_test.append(sentence_seq)
+
+    print("Ilość unik. nauczonych słów z train:", len(all_words_in_test))
+    print("Nieznane słowa z test:", len(unknown_words_in_test))
+
+    X3_train = pad_sequences(X3_train, padding='post', maxlen=maxlen)
+    X3_test = pad_sequences(X3_test, padding='post', maxlen=maxlen)
 
 
-""" TweetTokenizer embedding """
-embedding_matrix_2 = zeros((vocab_size_2, 100))
-for word, index in tweet_tokenizer_word_dict.items():
-    embedding_vector = embeddings_dictionary.get(word)
-    if embedding_vector is not None:
-        embedding_matrix_2[index] = embedding_vector
+    """ TreeBankTokenizer embedding """
+    embedding_matrix_3 = zeros((vocab_size_3, 100))
+    for word, index in nltk_tokenizer_word_dict.items():
+        embedding_vector = embeddings_dictionary.get(word)
+        if embedding_vector is not None:
+            embedding_matrix_3[index] = embedding_vector
 
-print(len(embedding_matrix_2))
-sys.exit(0)
 
-# """ TweetTokenizer embedding """
-# embedding_matrix_3 = zeros((vocab_size_3, 100))
-# for word, index in nltk_tokenizer_word_dict.items():
-#     embedding_vector = embeddings_dictionary.get(word)
-#     if embedding_vector is not None:
-#         embedding_matrix_3[index] = embedding_vector
+    # sys.exit(0)
+    ######################
+elif chosen_tokenizer == 'tt':
+
+    """ TweetTokenizer NLTK """
+    word_index = 1
+    tweet_tokenizer_word_dict = {}
+    X2_train = []
+    X2_test = []
+    tweettokenizer = TweetTokenizer(strip_handles=True, reduce_len=True)
+    for sen in X1_train_clean:
+        sentence_seq = []
+        sen_tokens = tweettokenizer.tokenize(sen)
+        for token in sen_tokens:
+            if token not in tweet_tokenizer_word_dict:
+                tweet_tokenizer_word_dict[token] = word_index
+                word_index += 1
+
+            sentence_seq.append(tweet_tokenizer_word_dict[token])
+        X2_train.append(sentence_seq)
+
+    vocab_size_2 = len(tweet_tokenizer_word_dict) + 1
+    # print(tweet_tokenizer_word_dict)
+
+    unknown_words_in_test = set()
+    all_words_in_test = set()
+    for sen in X1_test_clean:
+        sentence_seq = []
+        sen_tokens = tweettokenizer.tokenize(sen)
+        for token in sen_tokens:
+            all_words_in_test.add(token)
+            try:
+                sentence_seq.append(tweet_tokenizer_word_dict[token])
+            except:
+                unknown_words_in_test.add(token)
+                # jesli nie znam słowa nic nie dodaje
+        X2_test.append(sentence_seq)
+
+    print("Ilość unik. nauczonych słów z train:", len(all_words_in_test))
+    print("Nieznane słowa z test:", len(unknown_words_in_test))
+    X2_train = pad_sequences(X2_train, padding='post', maxlen=maxlen)
+    X2_test = pad_sequences(X2_test, padding='post', maxlen=maxlen)
+
+
+    """ TweetTokenizer embedding """
+    embedding_matrix_2 = zeros((vocab_size_2, 100))
+    for word, index in tweet_tokenizer_word_dict.items():
+        embedding_vector = embeddings_dictionary.get(word)
+        if embedding_vector is not None:
+            embedding_matrix_2[index] = embedding_vector
+
+    # print(len(embedding_matrix_2))
+    # sys.exit(0)
 
 
 """
     NN Model 1 input
 """
 deep_inputs = Input(shape=(maxlen,))
-# embedding_layer = Embedding(vocab_size, 100, weights=[embedding_matrix], trainable=False)(deep_inputs)
-embedding_layer = Embedding(vocab_size_2, 100, weights=[embedding_matrix_2], trainable=False)(deep_inputs)
-# embedding_layer = Embedding(vocab_size_3, 100, weights=[embedding_matrix_3], trainable=False)(deep_inputs)
+if chosen_tokenizer == 'keras':
+    embedding_layer = Embedding(vocab_size, 100, weights=[embedding_matrix], trainable=False)(deep_inputs)
+elif chosen_tokenizer == 'tt':
+    embedding_layer = Embedding(vocab_size_2, 100, weights=[embedding_matrix_2], trainable=False)(deep_inputs)
+elif chosen_tokenizer == 'tbwt':
+    embedding_layer = Embedding(vocab_size_3, 100, weights=[embedding_matrix_3], trainable=False)(deep_inputs)
 
 LSTM_Layer_1 = LSTM(128)(embedding_layer)
 dense_layer_1 = Dense(2, activation='softmax')(LSTM_Layer_1)
 model = Model(inputs=deep_inputs, outputs=dense_layer_1)
-
+# RELU
+# backpropagation
+# stochasting descent
+# loss function
+# dropout - technika regulacyjna
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
 print(model.summary())
 
 plot_model(model, to_file='model_plot1.png', show_shapes=True, show_layer_names=True)
-# history = model.fit(X1_train, y_train, batch_size=128, epochs=10, verbose=1, validation_split=0.2)
-# score = model.evaluate(X1_test, y_test, verbose=1)
-history = model.fit(X2_train, y_train, batch_size=128, epochs=10, verbose=1, validation_split=0.2)
-score = model.evaluate(X2_test, y_test, verbose=1)
-# history = model.fit(X3_train, y_train, batch_size=128, epochs=10, verbose=1, validation_split=0.2)
-# score = model.evaluate(X3_test, y_test, verbose=1)
+if chosen_tokenizer == 'keras':
+    history = model.fit(X1_train, y_train, batch_size=128, epochs=10, verbose=1, validation_split=0.2)
+    score = model.evaluate(X1_test, y_test, verbose=1)
+elif chosen_tokenizer == 'tt':
+    history = model.fit(X2_train, y_train, batch_size=128, epochs=10, verbose=1, validation_split=0.2)
+    score = model.evaluate(X2_test, y_test, verbose=1)
+elif chosen_tokenizer == 'tbwt':
+    history = model.fit(X3_train, y_train, batch_size=128, epochs=10, verbose=1, validation_split=0.2)
+    score = model.evaluate(X3_test, y_test, verbose=1)
 
 print("Test Score:", score[0])
 print("Test Accuracy:", score[1])
