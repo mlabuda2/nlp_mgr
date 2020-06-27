957144ebbaf8369d1429016511e4802010448d49
mlabuda2 <labuda1996@wp.pl>
2020-06-27 09:23:18 +0200

    configurations

---
+++ tweet_sentiment/bow_test.py
@@ -0,0 +1,169 @@
+from numpy import asarray
+import itertools
+
+import matplotlib
+import matplotlib.pyplot as plt
+import nltk
+import pandas as pd
+import seaborn as sns
+from keras import optimizers
+from keras.layers import Input
+from keras.layers import LSTM
+from keras.layers.core import Dropout, Dense
+from keras.layers.embeddings import Embedding
+from keras.models import Model
+from keras.preprocessing.sequence import pad_sequences
+from keras.preprocessing.text import Tokenizer
+from keras.utils import plot_model
+from keras.utils import to_categorical
+from nltk.corpus import stopwords
+from nltk.stem.lancaster import *
+from nltk.tokenize import TweetTokenizer, word_tokenize
+from numpy import asarray
+from numpy import zeros
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.model_selection import train_test_split
+
+from sklearn.datasets import fetch_20newsgroups
+
+import numpy as np
+
+import keras
+
+from keras.models import Model
+from keras.layers import Dense, Activation, concatenate, Embedding, Input
+
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+
+# https://www.kaggle.com/kazanova/sentiment140
+# https://www.kaggle.com/paoloripamonti/twitter-sentiment-analysis
+dataset = pd.read_csv("/home/mati/nlp_mgr/tweet_sentiment/training.1600000.processed.noemoticon.csv",
+                         header=None,
+                         skiprows=790000, nrows=20000,
+                         # skiprows=780000, nrows=40000,
+                         # skiprows=650000, nrows=300000,
+                        #  skiprows=500000, nrows=600000,
+                         encoding='latin1')
+dataset.columns = ['target','ids','date','flag','user','text']
+bins = [-1, 2, 4]
+review_names = ['bad', 'good']
+dataset['reviews_score'] = pd.cut(dataset['target'], bins, labels=review_names)
+
+dataset.isnull().values.any()
+print(dataset.shape)
+
+print("====== Lemmatizer ======")
+ps = nltk.stem.WordNetLemmatizer()
+dataset["text"] = dataset["text"].apply(lambda x: ' '.join([ps.lemmatize(word) for word in x.split()]))
+
+X = dataset.drop('reviews_score', axis=1)
+y = dataset['reviews_score']
+
+# label_encoder object knows how to understand word labels.
+label_encoder = preprocessing.LabelEncoder()
+
+# Encode labels in column 'species'.
+y = label_encoder.fit_transform(y)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
+
+y_train = to_categorical(y_train, num_classes=2)
+y_test = to_categorical(y_test, num_classes=2)
+
+X_train = list(X_train["text"])
+X_test = list(X_test["text"])
+
+
+words = list(set([word for doc in X_train for word in doc.lower().split()]))
+vectors = []
+for sen in X_train:
+    vectors.append([1 if word in sen.lower().split() else 0 for word in words])
+
+for vector in vectors:
+    vector = vector[:300]
+
+# # create the transform
+# vectorizer = CountVectorizer(max_features=300)
+# # tokenize and build vocab
+# vectorizer.fit(X_train)
+#
+# df_train = vectorizer.transform(X_train)
+# df_test = vectorizer.transform(X_test)
+
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(X_train)
+
+maxlen = 50
+
+sequences_train = tokenizer.texts_to_sequences(X_train)
+sequences_train = pad_sequences(sequences_train, padding='post', maxlen=maxlen)
+
+sequences_test = tokenizer.texts_to_sequences(X_test)
+sequences_test = pad_sequences(sequences_test, padding='post',  maxlen=maxlen)
+
+vocab_size = len(tokenizer.word_index) + 1
+embedding_size = 300
+
+input_tfidf = Input(shape=(300,))
+# input_tfidf = Input(shape=df_train.shape[1:])
+input_text = Input(shape=(maxlen,))
+
+embedding = Embedding(vocab_size, embedding_size, input_length=maxlen)(input_text)
+
+# this averaging method taken from:
+# https://stackoverflow.com/a/54217709/1987598
+
+# mean_embedding = keras.layers.Lambda(lambda x: keras.backend.mean(x, axis=1))(embedding)
+# concatenated = concatenate([input_tfidf, mean_embedding])
+dropout_layer = Dropout(0.1)(input_tfidf)
+dense1 = Dense(256, activation='relu')(dropout_layer)
+dense2 = Dense(32, activation='relu')(dense1)
+dense3 = Dense(2, activation='softmax')(dense2)
+model = Model(inputs=input_tfidf, outputs=dense3)
+# model = Model(inputs=[input_tfidf, input_text], outputs=dense3)
+
+# model 0
+# dropout_layer = Dropout(0.1)(input_tfidf)
+# LSTM_Layer_1 = LSTM(128, dropout=0.15, recurrent_dropout=0.15)(input_tfidf)
+# dense_layer_1 = Dense(64, activation='relu')(LSTM_Layer_1)
+# dense_layer_2 = Dense(2, activation='softmax')(dense_layer_1)
+# model = Model(inputs=input_tfidf, outputs=dense_layer_2)
+
+model.summary()
+
+adam = optimizers.Adam(learning_rate=0.001, beta_1=0.9, beta_2=0.999, amsgrad=False)
+model.compile(optimizer=adam, loss='categorical_crossentropy', metrics=['acc'])
+
+
+# history = model.fit([df_train, sequences_train], y_train, batch_size=128, epochs=7, verbose=1, validation_split=0.2)
+# score = model.evaluate([df_test, sequences_test], y_test, verbose=1)
+
+plot_model(model, to_file='model_plot2.png', show_shapes=True, show_layer_names=True)
+
+
+history = model.fit(df_train, y_train, batch_size=128, epochs=7, verbose=1, validation_split=0.2)
+score = model.evaluate(df_test, y_test, verbose=1)
+
+print("Loss:", score[0])
+print("Accuracy:", score[1])
+
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
--- tweet_sentiment/with_stemmer.py
+++ tweet_sentiment/with_stemmer.py
@@ -1,40 +1,48 @@
-from numpy import array
 from numpy import asarray
-from numpy import zeros
-from numpy import array
-from keras.preprocessing.text import one_hot
-from keras.preprocessing.sequence import pad_sequences
-from keras.models import Sequential
-from keras.layers.core import Activation, Dropout, Dense
-from keras.layers import Flatten, LSTM
-from keras.layers import GlobalMaxPooling1D
-from keras.models import Model
-from keras.layers.embeddings import Embedding
-from sklearn.model_selection import train_test_split
-from keras.preprocessing.text import Tokenizer
-from keras.layers import Input
-from keras.layers.merge import Concatenate
-from keras.utils import to_categorical
-from keras.utils import plot_model
-from keras import optimizers
-from sklearn import preprocessing
-import nltk
-from nltk.tokenize import TweetTokenizer, word_tokenize
-import pandas as pd
-import numpy as np
-import re
-import sys
-import seaborn as sns
-import sys
-import matplotlib
-import matplotlib.pyplot as plt
-matplotlib.use('TkAgg')
-import nltk
-from nltk.stem.lancaster import *
 import itertools
 
+import matplotlib
+import matplotlib.pyplot as plt
+import nltk
+import pandas as pd
+import seaborn as sns
+from keras import optimizers
+from keras.layers import Input
+from keras.layers import LSTM
+from keras.layers.core import Dropout, Dense
+from keras.layers.embeddings import Embedding
+from keras.models import Model
+from keras.preprocessing.sequence import pad_sequences
+from keras.preprocessing.text import Tokenizer
+from keras.utils import plot_model
+from keras.utils import to_categorical
+from nltk.corpus import stopwords
+from nltk.stem.lancaster import *
+from nltk.tokenize import TweetTokenizer, word_tokenize
+from numpy import asarray
+from numpy import zeros
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+
+matplotlib.use('TkAgg')
+
+# config on/off
+chosen_tokenizer = 'KerasTokenizer'
+lemmatizer_on = False
+lemmatizer = 'WordNetLemmatizer'
+stopwords_on = False
+stemmer_on = False
+stemmer = 'PorterStemmer'
+sentence_preprocessing = False
+custom_embedding_on = False
+embedding = 'Glove'
+
+show_data_division = False
+
 def preprocess_text(sen):
-    # lowercase all
+    if not sentence_preprocessing:
+        return sen
+    # Lowercase all
     sen = sen.lower()
 
     # Remove punctuations and numbers
@@ -59,11 +67,12 @@ def preprocess_text(sen):
 
 
 # https://www.kaggle.com/kazanova/sentiment140
+# https://www.kaggle.com/paoloripamonti/twitter-sentiment-analysis
 dataset = pd.read_csv("/home/mati/nlp_mgr/tweet_sentiment/training.1600000.processed.noemoticon.csv",
                          header=None,
-                         # skiprows=790000, nrows=20000,
+                         skiprows=790000, nrows=20000,
                          # skiprows=780000, nrows=40000,
-                         skiprows=650000, nrows=300000,
+                         # skiprows=650000, nrows=300000,
                         #  skiprows=500000, nrows=600000,
                          encoding='latin1')
 dataset.columns = ['target','ids','date','flag','user','text']
@@ -74,17 +83,36 @@ dataset['reviews_score'] = pd.cut(dataset['target'], bins, labels=review_names)
 dataset.isnull().values.any()
 print(dataset.shape)
 
-dataset.head()
-print(dataset["text"][:15])
-ps = nltk.stem.WordNetLemmatizer()
-print("====== Lemmatizer ======")
-dataset["text"] = dataset["text"].apply(lambda x: ' '.join([ps.lemmatize(word) for word in x.split()]))
-print(dataset["text"][:15])
+print(dataset.head())
+
+if stemmer_on:
+    print("====== Stemmer ======")
+    if stemmer == 'PorterStemmer':
+        pst = nltk.stem.PorterStemmer()
+    elif stemmer == 'LancasterStemmer':
+        pst = nltk.stem.LancasterStemmer()
+    else:
+        raise("Not found stemmer")
+    dataset["text"] = dataset["text"].apply(lambda x: ' '.join([pst.stem(word) for word in x.split()]))
+if lemmatizer_on:
+    print("====== Lemmatizer ======")
+    if lemmatizer == 'WordNetLemmatizer':
+        ps = nltk.stem.WordNetLemmatizer()
+        dataset["text"] = dataset["text"].apply(lambda x: ' '.join([ps.lemmatize(word) for word in x.split()]))
+    else:
+        raise('Not found Lemmatizer')
+        sys.exit(0)
+if stopwords_on:
+    print("====== Removing stopwords ======")
+    stop = stopwords.words('english')
+    dataset["text"] = dataset["text"].apply(lambda x: ' '.join([word for word in x.split() if word not in stop]))
+
 dataset["Text Length"] = dataset["text"].str.len()
 print(dataset["Text Length"].mean())
 
-# sns.countplot(x='reviews_score', data=dataset)
-# plt.show()
+if show_data_division:
+    sns.countplot(x='reviews_score', data=dataset)
+    plt.show()
 
 X = dataset.drop('reviews_score', axis=1)
 y = dataset['reviews_score']
@@ -115,28 +143,24 @@ for sen in X1_test_sentences:
 
 X1_test_clean = list(X1_test)
 
+if custom_embedding_on and embedding == 'Glove':
+    embeddings_dictionary = dict()
+    glove_file = open('/home/mati/nlp_mgr/glove.twitter.27B/glove.twitter.27B.100d.txt', encoding="utf8")
+    for line in glove_file:
+        records = line.split()
+        word = records[0]
+        vector_dimensions = asarray(records[1:], dtype='float32')
+        embeddings_dictionary[word] = vector_dimensions
 
-embeddings_dictionary = dict()
-# http://nlp.stanford.edu/data/wordvecs/glove.twitter.27B.zip
-glove_file = open('/home/mati/nlp_mgr/glove/glove.twitter.27B/glove.twitter.27B.100d.txt', encoding="utf8")
-for line in glove_file:
-    records = line.split()
-    word = records[0]
-    vector_dimensions = asarray(records[1:], dtype='float32')
-    embeddings_dictionary[word] = vector_dimensions
-
-glove_file.close()
+    glove_file.close()
 
 unknown_words_in_test = set()
 all_words_in_test = set()
-maxlen = 50
-chosen_tokenizer = 'keras'
-if chosen_tokenizer == 'keras':
+maxlen = 45
+if chosen_tokenizer == 'KerasTokenizer':
     """ Keras Tokenizer """
-    # """
-    tokenizer = Tokenizer(num_words=10000)
+    tokenizer = Tokenizer(num_words=10000, lower=True, filters='!"#$%&*+,.?[\\]`{|}~\t\n')
     tokenizer.fit_on_texts(X1_train)
-    # print(tokenizer.word_index)
     X1_train = tokenizer.texts_to_sequences(X1_train)
     X1_test = tokenizer.texts_to_sequences(X1_test)
     vocab_size = len(tokenizer.word_index) + 1
@@ -144,19 +168,18 @@ if chosen_tokenizer == 'keras':
     X1_train = pad_sequences(X1_train, padding='post', maxlen=maxlen)
     X1_test = pad_sequences(X1_test, padding='post', maxlen=maxlen)
 
-    """ Keras tokenizer embedding """
-    embedding_matrix = zeros((vocab_size, 100))
-    for word, index in tokenizer.word_index.items():
-        embedding_vector = embeddings_dictionary.get(word)
-        if embedding_vector is not None:
-            embedding_matrix[index] = embedding_vector
-        else:
-            unknown_words_in_test.add(word)
-    # print("Ilość unik. nauczonych słów z train:", len(all_words_in_test))
-    print("Nieznane słowa z test:", len(unknown_words_in_test))
-    ###########
-    # """
-elif chosen_tokenizer == 'tbwt':
+    if custom_embedding_on and embedding == 'Glove':
+        print(""" Keras tokenizer embedding """)
+        embedding_matrix = zeros((vocab_size, 100))
+        for word, index in tokenizer.word_index.items():
+            embedding_vector = embeddings_dictionary.get(word)
+            if embedding_vector is not None:
+                embedding_matrix[index] = embedding_vector
+            else:
+                unknown_words_in_test.add(word)
+        # print("Ilość unik. nauczonych słów z train:", len(all_words_in_test))
+        print("Nieznane słowa z test:", len(unknown_words_in_test))
+elif chosen_tokenizer == 'TreeBankTokenizer':
     """ TreebankWordTokenizer NLTK """
     word_index = 1
     nltk_tokenizer_word_dict = {}
@@ -195,19 +218,14 @@ elif chosen_tokenizer == 'tbwt':
     X3_train = pad_sequences(X3_train, padding='post', maxlen=maxlen)
     X3_test = pad_sequences(X3_test, padding='post', maxlen=maxlen)
 
-
-    """ TreeBankTokenizer embedding """
-    embedding_matrix_3 = zeros((vocab_size_3, 100))
-    for word, index in nltk_tokenizer_word_dict.items():
-        embedding_vector = embeddings_dictionary.get(word)
-        if embedding_vector is not None:
-            embedding_matrix_3[index] = embedding_vector
-
-
-    # sys.exit(0)
-    ######################
-elif chosen_tokenizer == 'tt':
-
+    if custom_embedding_on and embedding == 'Glove':
+        print(""" TreeBankTokenizer embedding """)
+        embedding_matrix_3 = zeros((vocab_size_3, 100))
+        for word, index in nltk_tokenizer_word_dict.items():
+            embedding_vector = embeddings_dictionary.get(word)
+            if embedding_vector is not None:
+                embedding_matrix_3[index] = embedding_vector
+elif chosen_tokenizer == 'TweetTokenizer':
     """ TweetTokenizer NLTK """
     word_index = 1
     tweet_tokenizer_word_dict = {}
@@ -226,7 +244,6 @@ elif chosen_tokenizer == 'tt':
         X2_train.append(sentence_seq)
 
     vocab_size_2 = len(tweet_tokenizer_word_dict) + 1
-    # print(tweet_tokenizer_word_dict)
 
     unknown_words_in_test = set()
     all_words_in_test = set()
@@ -247,53 +264,61 @@ elif chosen_tokenizer == 'tt':
     X2_train = pad_sequences(X2_train, padding='post', maxlen=maxlen)
     X2_test = pad_sequences(X2_test, padding='post', maxlen=maxlen)
 
-
-    """ TweetTokenizer embedding """
-    embedding_matrix_2 = zeros((vocab_size_2, 100))
-    for word, index in tweet_tokenizer_word_dict.items():
-        embedding_vector = embeddings_dictionary.get(word)
-        if embedding_vector is not None:
-            embedding_matrix_2[index] = embedding_vector
-
-    # print(len(embedding_matrix_2))
-    # sys.exit(0)
-
+    if custom_embedding_on and embedding == 'Glove':
+        print(""" TweetTokenizer embedding """)
+        embedding_matrix_2 = zeros((vocab_size_2, 100))
+        for word, index in tweet_tokenizer_word_dict.items():
+            embedding_vector = embeddings_dictionary.get(word)
+            if embedding_vector is not None:
+                embedding_matrix_2[index] = embedding_vector
 
 """
     NN Model 1 input
 """
 deep_inputs = Input(shape=(maxlen,))
-if chosen_tokenizer == 'keras':
-    embedding_layer = Embedding(vocab_size, 100, weights=[embedding_matrix])(deep_inputs)
-elif chosen_tokenizer == 'tt':
+if not custom_embedding_on:
+    embedding_layer = Embedding(vocab_size, 100, input_length=maxlen)(deep_inputs)
+elif chosen_tokenizer == 'KerasTokenizer':
+    embedding_layer = Embedding(vocab_size, 100, weights=[embedding_matrix], trainable=False)(deep_inputs)
+elif chosen_tokenizer == 'TweetTokenizer':
     embedding_layer = Embedding(vocab_size_2, 100, weights=[embedding_matrix_2], trainable=False)(deep_inputs)
-elif chosen_tokenizer == 'tbwt':
+elif chosen_tokenizer == 'TreeBankTokenizer':
     embedding_layer = Embedding(vocab_size_3, 100, weights=[embedding_matrix_3], trainable=False)(deep_inputs)
 
-LSTM_Layer_1 = LSTM(128)(embedding_layer)
+# model 0
+dropout_layer = Dropout(0.3)(embedding_layer)
+LSTM_Layer_1 = LSTM(128, dropout=0.15, recurrent_dropout=0.15)(dropout_layer)
 dense_layer_1 = Dense(2, activation='softmax')(LSTM_Layer_1)
 model = Model(inputs=deep_inputs, outputs=dense_layer_1)
+EPOCHS_COUNT = 7
 
-# model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
-adam = optimizers.Adam(learning_rate=0.0005, beta_1=0.9, beta_2=0.999, amsgrad=False)
+# model 1
+# LSTM_Layer_1 = LSTM(128)(embedding_layer)
+# dense_layer_1 = Dense(2, activation='softmax')(LSTM_Layer_1)
+# model = Model(inputs=deep_inputs, outputs=dense_layer_1)
+# EPOCHS_COUNT = 5
+
+adam = optimizers.Adam(learning_rate=0.001, beta_1=0.9, beta_2=0.999, amsgrad=False)
 model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=['acc'])
 
 print(model.summary())
 
 plot_model(model, to_file='model_plot1.png', show_shapes=True, show_layer_names=True)
-if chosen_tokenizer == 'keras':
-    history = model.fit(X1_train, y_train, batch_size=128, epochs=6, verbose=1, validation_split=0.2)
+if not custom_embedding_on:
+    history = model.fit(X1_train, y_train, batch_size=128, epochs=EPOCHS_COUNT, verbose=1, validation_split=0.2)
     score = model.evaluate(X1_test, y_test, verbose=1)
-elif chosen_tokenizer == 'tt':
-    history = model.fit(X2_train, y_train, batch_size=128, epochs=10, verbose=1, validation_split=0.2)
+elif chosen_tokenizer == 'KerasTokenizer':
+    history = model.fit(X1_train, y_train, batch_size=128, epochs=EPOCHS_COUNT, verbose=1, validation_split=0.2)
+    score = model.evaluate(X1_test, y_test, verbose=1)
+elif chosen_tokenizer == 'TweetTokenizer':
+    history = model.fit(X2_train, y_train, batch_size=128, epochs=EPOCHS_COUNT, verbose=1, validation_split=0.2)
     score = model.evaluate(X2_test, y_test, verbose=1)
-elif chosen_tokenizer == 'tbwt':
-    history = model.fit(X3_train, y_train, batch_size=128, epochs=10, verbose=1, validation_split=0.2)
+elif chosen_tokenizer == 'TreeBankTokenizer':
+    history = model.fit(X3_train, y_train, batch_size=128, epochs=EPOCHS_COUNT, verbose=1, validation_split=0.2)
     score = model.evaluate(X3_test, y_test, verbose=1)
 
-print("Test Score:", score[0])
-print("Test Accuracy:", score[1])
-
+print("Loss:", score[0])
+print("Accuracy:", score[1])
 
 plt.plot(history.history['acc'])
 plt.plot(history.history['val_acc'])
